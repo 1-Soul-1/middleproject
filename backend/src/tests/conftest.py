08fa3ab974b60048dc215c36782cc4d551abeef1
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-01-21 10:10:01 +0500

    create-migrations-db-(sqllite)

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -1,1 +1,10 @@
-# Фикс для pytest
+# создаём тестовую модель продукта 
+import pytest 
+from product.models import Product
+
+@pytest.fixture 
+def sample_product():
+    return Product.objects.create(
+        name = "Test Product",
+        price = 100.00,
+        in_stock = True
