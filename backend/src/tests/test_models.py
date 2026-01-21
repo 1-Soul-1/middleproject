98833996c046f8a94e709aa8b26f7015ba318bb3
1-Soul-1 <ruslanpupkin789@gmail.com>
2026-01-21 10:14:42 +0500

    create-pytest-models-product

---
+++ backend/src/tests/test_models.py
@@ -0,0 +1,14 @@
+import pytest 
+import pytest 
+from product.models import Product
+
+# тест 1 проверяем создание продукта 
+def test_product_creation():
+    product = Product.objects.create(
+        name = "Apples",
+        price = 999.99,
+        in_stoct = True
+    )
+    assert product.name == "Apples"
+    assert product.price == 999.99
+    assert product.in_stock == True
